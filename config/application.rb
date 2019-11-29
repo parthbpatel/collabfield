0f6a7a9b059ede62869881e918b524260821e10b
parthbpatel <parthtech3010@gmail.com>
2019-11-29 18:46:53 +0530

    navbar comment

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Collabfield
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.action_controller.include_all_helpers = false
+    # config.action_controller.include_all_helpers = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
