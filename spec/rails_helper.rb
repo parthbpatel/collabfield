40beace1f3b22d6d0ff79bc0e2da203cf1c2d88b
parthbpatel <parthtech3010@gmail.com>
2019-12-02 12:57:38 +0530

    add logout feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,8 +12,8 @@ require 'capybara/poltergeist'
 require 'factory_bot'
 require 'capybara/rspec'
 
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::Methods
+# config.include Devise::Test::IntegrationHelpers, type: :feature
+config.include FactoryBot::Syntax::Methods
 Capybara.javascript_driver = :poltergeist
 Capybara.server = :puma
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,8 +50,8 @@ RSpec.configure do |config|
   require 'factory_bot'
   require 'capybara/rspec'
 
-  config.include Devise::Test::IntegrationHelpers, type: :feature
-  config.include FactoryGirl::Syntax::Methods
+  # config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
   Capybara.javascript_driver = :poltergeist
   config.use_transactional_fixtures = false
   config.before(:suite) do
