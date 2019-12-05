c15804bdb1827b1e32cf1e3143bb6f7223ad3603
parthbpatel <parthtech3010@gmail.com>
2019-12-05 17:34:08 +0530

    Render a drop down menu of conversations links - split layouts/navigation/_header.html.erb file's content into partials - Create a _toggle_button.erb.html inside layouts/navigation/header - Create a _home_button.html.erb inside  layouts/navigation/header - Define a nav_header_content_partials inside NavigationHelper   and write specs for it - Create a _dropdowns.html.erb inside layouts/navigation/header - Create a _conversation.html.erb inside   layouts/navigation/header/dropdowns - Define a conversation_header_partial_path inside NavigationHelper   and write specs for it - Create a  _private.html.erb inside   layouts/navigation/header/dropdowns/conversations - Define a private_conv_seen_status inside   Shared::ConversationsHelper and write specs for it - Define an open action inside the Private::Conversations controller - add CSS to style drop down menus on the navigation bar.   Inside navigation.scss, mobile.scss and desktop.scss

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -24,7 +24,15 @@ class Private::ConversationsController < ApplicationController
     respond_to do |format|
       format.js
     end
-  end  
+  end
+
+  def open
+    @conversation = Private::Conversation.find(params[:id])
+    add_to_conversations unless already_added?
+      respond_to do |format|
+        format.js { render partial: 'private/conversations/open' }
+    end
+  end
 
   private
 
